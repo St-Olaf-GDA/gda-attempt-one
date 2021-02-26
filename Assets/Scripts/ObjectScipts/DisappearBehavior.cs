f80dd4d3b447c985b8504c1ade5f6fe951c8e699
syang <syang317@gmail.com>
2021-02-25 20:06:54 -0600

    Organized script folders. Created mushroom and yellow platforms.

---
+++ Assets/Scripts/ObjectScipts/DisappearBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearBehavior : MonoBehaviour
+{
+
+    public bool crumble;
+    public bool destroyOnTouch;
+
+    private GameObject player;
+
+    private void Start() {
+        // If we want the platform to crumble, then we'll also need destroyOnTouch to be true
+        // This is just incase we forget to set destroyOnTouch to true
+        if (crumble) destroyOnTouch = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        // Detect player
+        if (player == null) player = GameObject.Find("Player");
+
+        if (collision.gameObject == player && destroyOnTouch) {
+            if (crumble) {
+                StartCoroutine("Crumble");
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator Crumble() {
+        // We need to have crumbling animation here
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
