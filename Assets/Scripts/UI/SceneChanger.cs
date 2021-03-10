2bcf312f68d3665780e6e839a29cdf13d5af8dab
Hydraheads <Hydraheads42@gmail.com>
2021-03-10 15:44:12 -0600

    Save System Complete
    When we get to a new screen, contact me and I will explain how to transition between scenes, it is already within our capabilities

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Made and Updated by Andrew
 public class SceneChanger : MonoBehaviour
 {
-	public void ChangeScene(string sceneName)
+	public void ChangeScene()
 	{
+    //sceneChanger will append this number to the end of
+    //the scene it loads, so please consider naming every
+    //scene after the level value (ex scene1 for level=1)
+		string sceneName = "scene"+ SaveData.instance.data.level.ToString();
 		SceneManager.LoadScene (sceneName);
 	}
 	public void Exit()
