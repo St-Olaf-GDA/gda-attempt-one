11c19ab19b725adb7248275a42a9b888befcbc0b
GeorgiosKokalas <kokalasg@gmail.com>
2021-04-07 18:34:06 -0500

    NPCs now account for gravity

--- Assets/Scripts/Player_Scripts/Player_Interactions.cs
+++ Assets/Scripts/Player_Scripts/Player_Interactions.cs
@@ -9,28 +9,47 @@ public class Player_Interactions : MonoBehaviour
     public GameObject currentInteractible = null;
     public GameObject interact_bubble;
 
+    [SerializeField] bool is_currently_interacting = false;
+
     public void check_for_interactions(){
         if(Input.GetButtonDown("Interact") && currentInteractible){
-            currentInteractible.SendMessage("Interact");
+            is_currently_interacting = currentInteractible.GetComponent<Interactible>().Interact(is_currently_interacting);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Talking_NPC")) {
-            currentInteractible = other.gameObject;
-            interact_bubble = currentInteractible.transform.Find("Dialogue_Icon").gameObject;
-            interact_bubble.SetActive(true);
-            Debug.Log(currentInteractible.name);
-        }        
+        Debug.Log(other.name);
+        switch (other.tag){
+/*            case "Talking_NPC":
+                currentInteractible = other.gameObject;
+                interact_bubble = currentInteractible.transform.Find("Dialogue_Icon").gameObject;
+                interact_bubble.SetActive(true);
+                break;*/
+            case "NPC_Interaction_Collider":
+                Debug.Log("Interaction Collider");
+                currentInteractible = other.transform.parent.gameObject;
+                interact_bubble = currentInteractible.transform.Find("Dialogue_Icon").gameObject;
+                interact_bubble.SetActive(true);
+                break;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other){
-        if (other.CompareTag("Talking_NPC")) {
-            if (other.gameObject == currentInteractible){
-                interact_bubble.SetActive(false);
-                interact_bubble = null;
-                currentInteractible = null;
-            }
+        switch (other.tag){
+/*            case "Talking_NPC":
+                if (other.gameObject == currentInteractible){
+                    interact_bubble.SetActive(false);
+                    interact_bubble = null;
+                    currentInteractible = null;
+                }
+                break;*/
+            case "NPC_Interaction_Collider":
+                if (other.transform.parent.gameObject == currentInteractible){
+                    interact_bubble.SetActive(false);
+                    interact_bubble = null;
+                    currentInteractible = null;
+                }
+                break;
         }
     }
 }
